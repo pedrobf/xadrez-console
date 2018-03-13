7558e8a31b812d511b6a7ccc920e9a1537267590
Pedro Ferraro <pedrobferraro@gmail.com>
2018-03-13 11:51:10 -0300

    comecando a implementar imprimir tabuleiro

---
+++ xadrez-console/Tela.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int x = 0; x < tab.linhas; x++)
+            {
+                for (int y = 0; y < tab.colunas; y++)
+                {
+                    if (tab.peca(x, y) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.peca(x, y) + " ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
